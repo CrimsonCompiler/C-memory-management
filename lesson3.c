3f6b2a1eb46ea8c765243a8fb33b084ec89ec735
CrimsonCompiler <toisiftasrik@gmail.com>
2025-12-02 23:37:53 +0600

    feat: Add interpreted quiz to lesson3.c covering undefined function call behavior.

---
+++ lesson3.c
@@ -0,0 +1,22 @@
+/*
+Interpreted Quiz
+
+Up until now, you've probably only worked with interpreted languages on Boot.dev. 
+Use the following interpreted (in this case, Python) code to answer the question.
+
+```
+print("starting")
+func_that_doesnt_exist("uh oh")
+print("finished")
+
+```
+
+Assume func_that_doesnt_exist is a function that truly does not exist.
+
+--- What will happen when the code runs? --- 
+a ) A stack trace due to an undefined function call
+b ) 'starting' prints then a stack trace due to the undefined function call then 'finished' prints
+c ) 'starting' prints then 'finished' prints
+
+Answer: b
+*/
