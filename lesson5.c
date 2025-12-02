671208b0c4d874126661c5342ce8e412f884af43
CrimsonCompiler <toisiftasrik@gmail.com>
2025-12-03 01:09:59 +0600

    feat: Add `lesson5.c` demonstrating C comment syntax and basic program output.

---
+++ lesson5.c
@@ -0,0 +1,41 @@
+/*
+
+Comments
+
+In C, there are two ways to write comments:
+
+// This is a single-line comment
+
+/*
+This is a multi-line comment
+I can just keep adding lines
+and it will still be a comment
+
+// /* and */ // are used to denote the beginning and end of a multi-line comment.
+
+// Assignment
+
+// Looks like someone on the team doesn't know how to terminate a comment.
+
+// Fix the bug.
+// Assignment
+
+// Looks like someone on the team doesn't know how to terminate a comment.
+
+// Fix the bug.
+
+#include <stdio.h>
+
+int main() {
+  /*
+    Sneklang is for nvim enjoyers
+    who want to write their own garbage
+    collectors instead of using off-the-shelf
+    solutions
+  */
+  
+  printf("i use sneklang btw\n");
+  printf("i use nvim btw\n");
+  printf("i use arch btw\n");
+  return 0;
+}
