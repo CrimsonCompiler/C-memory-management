7f6def6fdc85c7640efd023fe986179d7283bd1c
CrimsonCompiler <toisiftasrik@gmail.com>
2025-12-03 19:23:20 +0600

    feat: Implement `print_numbers` function to print numbers within a specified range.

---
+++ lesson20.c
@@ -0,0 +1,14 @@
+/*
+
+
+Assignment
+
+Implement the print_numbers prototyped in exercise.h that takes a starting number and an ending number and prints all the numbers in that range inclusive (using a for-loop).
+
+*/
+
+void print_numbers(int start, int end){
+    for(int i = start; i <= end; i++){
+        printf("%d\n", i);
+    }
+}
