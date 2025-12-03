0fa69b911468aba32865d0eb59dfe10f6f976900
CrimsonCompiler <toisiftasrik@gmail.com>
2025-12-03 19:08:19 +0600

    feat: Implement `get_average` function to calculate the average of three numbers.

---
+++ lesson14.c
@@ -0,0 +1,16 @@
+/*
+
+
+Assignment
+
+Write the get_average (mathematical average) function in exercise.c based on the function prototype expected in exercise.h.
+
+
+*/
+
+
+#include "exercise.h"
+// ?
+float get_average(int x, int y, int z){
+  return (x + y + z) / 3.0f;
+}
